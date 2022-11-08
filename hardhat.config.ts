96014cf22af74714f0719f3d8eff369adc8fb78f
MiAnClGr <MiAnClGr@gmail.com>
2022-11-08 21:26:02 +1000

    added optimzer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,34 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+
+	networks: {
+		hardhat: {
+		  	// allowUnlimitedContractSize: true,
+
+		}
+	},
+
+	solidity: {
+		version: "0.8.17",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 1
+			}
+		}
+	},
+
+	namedAccounts: {
+		deployer: 0,
+		alice: 1,
+		bob: 2,
+		carol: 3,
+		derrick: 4,
+	},
 };
 
 export default config;
