3c25b15e68dbc71fdbfcd51ff96af71e9a11ae53
MiAnClGr <MiAnClGr@gmail.com>
2022-11-08 21:23:48 +1000

    added deployment scripts

---
+++ deploy/deploy_Performance_Contract.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import { Contract } from "ethers";
+
+
+const func : DeployFunction = async (hre : HardhatRuntimeEnvironment) => {
+
+    const {deployments, getNamedAccounts} = hre
+    const {deploy} = deployments
+
+    const {deployer} = await getNamedAccounts()
+
+    await deploy("PerformanceContract", {
+        from: deployer,
+        log: true,
+    })
+}
+export default func
+
+func.tags = ["PerformanceContract"]
