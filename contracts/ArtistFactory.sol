187608d16d94d5d35acaef77dc11d82a4a1296da
MiAnClGr <MiAnClGr@gmail.com>
2022-11-13 09:06:35 +1000

    added an artist check

--- contracts/ArtistFactory.sol
+++ contracts/ArtistFactory.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity 0.8.17;
-
 import "./ArtistProfile.sol";
 
 contract ArtistFactory {
 
+    mapping(address => address) public ownerToArtist;
     mapping(string => address) public artistByName;
     mapping(address => string) public artistByAddress;
     mapping(address => address) public artistProfileToArtist;
@@ -13,19 +13,29 @@ contract ArtistFactory {
 
     address[] public Artists;
 
-    event NewArtist(address);
+    event Artist(address, string);
 
     function createArtist(string memory _artistName) external {
     
-        require(!doesArtistExist[_artistName], "Artist already Exists");
+        if(doesArtistExist[_artistName]){
+            artistCheck(_artistName);
+        }else{
+            ArtistProfile artistProfile = new ArtistProfile(_artistName, msg.sender);
+            ownerToArtist[msg.sender] = address(artistProfile);
+            artistByName[_artistName] = address(artistProfile);
+            artistByAddress[address(artistProfile)] = _artistName;
+            artistProfileToArtist[address(artistProfile)] = msg.sender;
+            doesArtistExist[_artistName] = true;
+            Artists.push(address(artistProfile));     
+
+            emit Artist(address(artistProfile), "new artist");
+        }
         
-        ArtistProfile artistProfile = new ArtistProfile(_artistName, msg.sender);
-        artistByName[_artistName] = address(artistProfile);
-        artistByAddress[address(artistProfile)] = _artistName;
-        artistProfileToArtist[address(artistProfile)] = msg.sender;
-        doesArtistExist[_artistName] = true;
-        Artists.push(address(artistProfile));
-        
-        emit NewArtist(address(artistProfile));
+    }
+
+    function artistCheck(string memory _artistName) internal {
+        address _artist = artistByName[_artistName];
+        require(ownerToArtist[msg.sender] == _artist, "you are not the artist owner");
+        emit Artist(_artist, "existing artist");
     }
 }
