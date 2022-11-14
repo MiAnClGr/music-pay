acc15add42fde4216e389b6e90b28f7ace2be4d8
MiAnClGr <MiAnClGr@gmail.com>
2022-11-14 20:51:31 +1000

    added back button

--- src/LoadingAndError/NotOwner.tsx
+++ src/LoadingAndError/NotOwner.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const NotOwner = () => {
+
+  const navigate = useNavigate()
+
+  const back = () => {
+    navigate("/CreateArtist")
+  }
+
   return (
     <div className='LoadingScreen'>
         <h1 className='HeaderText'>
         You are not the owner of this profile :(    
         </h1>
+        <br></br>
+        <button className='Back' onClick= {back}>Back</button>
     </div>
   )
 }
