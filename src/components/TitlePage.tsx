49c47ac935522f4bc66b74a4b60009a213ef9189
MiAnClGr <MiAnClGr@gmail.com>
2022-11-17 23:00:56 +1000

    update

--- src/components/TitlePage.tsx
+++ src/components/TitlePage.tsx
@@ -15,7 +15,7 @@ const TitlePage : FC<props> = ({provider, notConnected, setNotConnected}) => {
 
   return (
     <div className= 'TitlePage'>
-      <h1 className='HeaderText'>Music-Pay</h1>
+      <h1 className='Music-Pay-Title'>Music-Pay</h1>
       <ConnectWallet
       provider= {provider}
       notConnected= {notConnected}
