3bdb380e895b8211cc2d267b74d08e59006c223d
MiAnClGr <MiAnClGr@gmail.com>
2022-11-18 11:12:00 +1000

    added .env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,10 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "hardhat-deploy";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 
 const config: HardhatUserConfig = {
 
@@ -12,8 +16,11 @@ const config: HardhatUserConfig = {
 		},
 
 		goerli: {
-			url: "https://eth-goerli.g.alchemy.com/v2/Gw-Mfv6jFIx2Tqa2YnudVC0CiR2aXuJc",
-			accounts: [""]
+			url: process.env.GOERLI_URL || "",
+			accounts: !!process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+			// gas: 2100000,
+			// gasPrice: 8000000000,
+			saveDeployments: true,
 		}
 	},
 
