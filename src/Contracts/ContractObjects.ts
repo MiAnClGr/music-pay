5a5992e53c6efa6a886e959bc7834f718515bf91
MiAnClGr <MiAnClGr@gmail.com>
2022-12-11 15:30:19 +1000

    deployed to Mumbai

--- src/Contracts/ContractObjects.ts
+++ src/Contracts/ContractObjects.ts
@@ -10,10 +10,10 @@ export const signer : ethers.providers.JsonRpcSigner = provider.getSigner()
 
 /// Artist Factory
 
-export const ArtistFactoryAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const ArtistFactoryAddress = "0xc5D156eda6e84076b0FB3c0Aa1E3850Be8F129BD"
 export const ArtistFactoryContract : Contract = new ethers.Contract(ArtistFactoryAddress, ArtistFactoryABI, signer);
 
 /// Artist Profile 
 
-export const PerformanceContractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+export const PerformanceContractAddress = "0xAfd020F0EE217Ab6ad5bdbdAAbdE59a1bd7C32c7"
 export const PerformanceContract : Contract = new ethers.Contract(PerformanceContractAddress, PerformanceContractABI, signer);
