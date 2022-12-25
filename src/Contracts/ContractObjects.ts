ca883b49f32ab3f918f3957a54b3ce5faaca1c82
MiAnClGr <MiAnClGr@gmail.com>
2022-12-26 08:59:41 +1000

    added MockDai

--- src/Contracts/ContractObjects.ts
+++ src/Contracts/ContractObjects.ts
@@ -2,23 +2,28 @@ import {ethers} from 'ethers'
 import {Contract} from 'ethers'
 import ArtistFactoryABI from '../ABI/ArtistFactory'
 import PerformanceContractABI from "../ABI/PerformanceContract"
+import MockDaiABI from "../ABI/MockDai"
 
 declare var window: any
 
-const ArtistFactoryAddress = "0x2E38cEfBc721dFcF5d7dA9A4609d31D23d034C19"
-const PerformanceContractAddress = "0xCe145823a9C038E83DC893e1c97F0CC77b4Ef494"
+const MockDaiAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+const ArtistFactoryAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+const PerformanceContractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 
 let provider : ethers.providers.Web3Provider
 let signer : ethers.providers.JsonRpcSigner
+let MockDai : Contract
 let ArtistFactoryContract : Contract 
 let PerformanceContract : Contract
+
 
 
 if(window.ethereum != null) {
     provider  = new ethers.providers.Web3Provider(window.ethereum)
     signer = provider.getSigner()
+    MockDai = new ethers.Contract(MockDaiAddress, MockDaiABI, signer);
     ArtistFactoryContract = new ethers.Contract(ArtistFactoryAddress, ArtistFactoryABI, signer); 
     PerformanceContract = new ethers.Contract(PerformanceContractAddress, PerformanceContractABI, signer);
-}
+}   
 
-export {provider, signer, ArtistFactoryContract, PerformanceContract}
+export {provider, signer, ArtistFactoryContract, PerformanceContract, MockDai}
