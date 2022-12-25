0f0123234d7f323ab62e2f9a5038e495f2c99090
MiAnClGr <MiAnClGr@gmail.com>
2022-12-26 07:06:51 +1000

    added minting of tokens at deployment for testing

--- deploy/01_deploy_Mock_Dai.tsx
+++ deploy/01_deploy_Mock_Dai.tsx
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-import { Contract } from "ethers";
+import { Contract, utils } from "ethers";
 
 
 const func : DeployFunction = async (hre : HardhatRuntimeEnvironment) => {
@@ -11,10 +11,17 @@ const func : DeployFunction = async (hre : HardhatRuntimeEnvironment) => {
 
     const {deployer} = await getNamedAccounts()
 
+    const [userOne, userTwo] = await ethers.getSigners()
+
     await deploy("MockDai", {
         from: deployer,
         log: true
     })
+
+    const MockDai : Contract = await ethers.getContract("MockDai")
+
+    await MockDai.connect(userOne).mintDai(utils.parseEther('1000'))
+    await MockDai.connect(userTwo).mintDai(utils.parseEther("1000"))
 }
 export default func
 
