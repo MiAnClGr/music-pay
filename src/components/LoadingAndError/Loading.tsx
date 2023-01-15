25f3bf25a795102e8f7e15821a9d0db025637d84
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-01-15 17:40:01 +1000

    small change

--- src/components/LoadingAndError/Loading.tsx
+++ src/components/LoadingAndError/Loading.tsx
@@ -2,7 +2,10 @@ import React, { ReactElement } from 'react'
 
 const Loading = () : ReactElement => {
   return (
-    <div className= "LoadingScreen">
+    <div 
+    className= "LoadingScreen"
+    style={{marginTop: "18%"}}
+    >
       <h1 className='Loading'>Loading</h1>
     </div>
   )
