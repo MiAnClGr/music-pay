c52e44785f0e73f59b2d232720e6aea70b5e5da1
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-01-17 21:45:49 +1000

    added deployments conditions for different chains

--- deploy/01_deploy_Mock_Dai.tsx
+++ deploy/01_deploy_Mock_Dai.tsx
@@ -2,16 +2,38 @@ import { ethers } from "hardhat";
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 import { Contract, utils } from "ethers";
+import * as dotenv from "dotenv";
+
+dotenv.config()
+
+const { PRIVATE_KEY_2 } : any = process.env
 
 
 const func : DeployFunction = async (hre : HardhatRuntimeEnvironment) => {
 
-    const {deployments, getNamedAccounts} = hre
+    const {deployments, getNamedAccounts, getChainId} = hre
     const {deploy} = deployments
 
     const {deployer} = await getNamedAccounts()
 
+    const chainId = await getChainId()
+
+    console.log(chainId)
+
     const [userOne, userTwo] = await ethers.getSigners()
+
+    const provider = 
+    new ethers.providers.JsonRpcProvider(
+        process.env.GOERLI_URL
+        )
+
+    const accountTwo = 
+    new ethers.Wallet(
+        PRIVATE_KEY_2, 
+        provider
+    )
+
+    console.log(accountTwo.address)
 
     await deploy("MockDai", {
         from: deployer,
@@ -20,9 +42,22 @@ const func : DeployFunction = async (hre : HardhatRuntimeEnvironment) => {
 
     const MockDai : Contract = await ethers.getContract("MockDai")
 
-    await MockDai.connect(userOne).mintDai(utils.parseEther('1000'))
-    await MockDai.connect(userTwo).mintDai(utils.parseEther('1000'))
-}
+    console.log(MockDai.address)
+
+    if(chainId == "31337"){
+        await MockDai.connect(userOne).mintDai(utils.parseEther('1000'))
+        await MockDai.connect(userTwo).mintDai(utils.parseEther('1000'))
+    }
+  
+    if(chainId == "5" || chainId == "80001"){
+        console.log("accessed")
+        // await MockDai.mintDai(utils.parseEther('1000'))
+        await MockDai.connect(accountTwo).mintDai(utils.parseEther('1000'))
+
+        console.log(await MockDai.balanceOf(accountTwo.address))
+    }
+ 
+}   
 export default func
 
 func.tags = ["MockDai"]
