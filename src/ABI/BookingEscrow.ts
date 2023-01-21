f15708ba651682a355b0651c0ee136ae9df8b2a7
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-01-21 10:13:25 +1000

    updated ABI

--- src/ABI/BookingEscrow.ts
+++ src/ABI/BookingEscrow.ts
@@ -188,6 +188,19 @@ export default [
   },
   {
     "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
     "name": "gigNumber",
     "outputs": [
       {
