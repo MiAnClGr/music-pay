90c2df1d7d94e569fc2d8b21c5ce2b8e040daf93
Michael Grinstead <michael.a.grinstead@gmail.com>
2023-02-21 20:30:56 +1000

    updated addressess deployed to mumbai

--- src/Contracts/ContractObjects.ts
+++ src/Contracts/ContractObjects.ts
@@ -14,8 +14,8 @@ declare var window: any
 
 ///Mumbai
 
-// const MockDaiAddress = "0x1023acA55D6aA1A8Dd0AcA62C6d559EdD09486F8"
-// const ArtistFactoryAddress = "0x41A68550EcE02B3D223C4c168e045b335B2A098f"
+const MockDaiAddress = "0x8B0AEe55bc82a7D622aA30b0d0249365B637b211"
+const ArtistFactoryAddress = "0xeBFe8D64A53d24185cef0e2b558126F58D631494"
 
 
 ///Polygon ZKEVM
@@ -26,8 +26,8 @@ declare var window: any
 
 ///LocalHost
 
-const MockDaiAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-const ArtistFactoryAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+// const MockDaiAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+// const ArtistFactoryAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
 
 
 
